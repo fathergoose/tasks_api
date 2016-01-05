a1447d24fbc5b71bb1784faafe64a5a2f3e66cbb
fathergoose <al@ilseman.com>
2016-01-05 15:36:17 -0600

    have directives and models

--- app/assets/javascripts/tasks-ctl.js
+++ app/assets/javascripts/tasks-ctl.js
@@ -2,6 +2,12 @@
   "use strict";
 
   angular.module("app").controller("tasksCtl", function($scope) {
-    $scope.message = "HELLO!!";
+    $scope.message = "HELLO!!";// the $scope.whatever thing is called a model so it can be used in the html
+                               // can also do $scope.somevar = function(arg1, arg2) to make functions
+    $scope.tasks = [
+    {
+      "key": "value",
+    }
+    ];
   });
 })();
