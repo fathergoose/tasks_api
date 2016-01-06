9d6ff6141afde7d7112d89e16ebe576cca14b2d4
fathergoose <al@ilseman.com>
2016-01-05 18:30:28 -0600

    api + front-end is hooked up

--- app/assets/javascripts/tasks-ctl.js
+++ app/assets/javascripts/tasks-ctl.js
@@ -1,13 +1,20 @@
 (function() {
   "use strict";
 
-  angular.module("app").controller("tasksCtl", function($scope) {
+  angular.module("app").controller("tasksCtl", function($scope, $http) {
     $scope.message = "HELLO!!";// the $scope.whatever thing is called a model so it can be used in the html
                                // can also do $scope.somevar = function(arg1, arg2) to make functions
-    $scope.tasks = [
-    {
-      "key": "value",
-    }
-    ];
+
+
+    $scope.setup = function() {
+      console.log("hello");
+      $http.get('api/v1/tasks.json').then(function(response) {
+        $scope.tasks = response.data;
+        console.log($scope.tasks);
+      })
+      
+    };
+    
+  
   });
 })();
